880de3b730be2aa0e003c15a8e02e139b01fc2e0
Fabolo <fabolo1234@gmail.com>
2018-04-20 17:40:34 +0300

    Added test page and move event to components

--- src/main/java/com/bottle/web/settings/DefaultView.java
+++ src/main/java/com/bottle/web/settings/DefaultView.java
@@ -13,8 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class DefaultView extends WebMvcConfigurerAdapter implements WebMvcConfigurer {
     @Override
     public void addViewControllers( ViewControllerRegistry registry ) {
-        //registry.addViewController( "/" ).setViewName( "forward:/home/home.html" );
-        registry.addViewController( "/" ).setViewName( "forward:/index.html" );
+        registry.addViewController( "/" ).setViewName( "forward:index.html" );
         registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
         super.addViewControllers( registry );
     }
