cbcc55625752a268acdda710c536b68ce42a1245
Fabolo <fabolo1234@gmail.com>
2018-04-23 18:53:58 +0300

    Add event to back and show they in front

--- src/main/resources/static/components/events/events.component.js
+++ src/main/resources/static/components/events/events.component.js
@@ -2,48 +2,49 @@
 
 angular.module('events').component('events', {
     templateUrl: 'components/events/events.template.html',
-    controller: ['$routeParams', 'EventFactory',
-        function UserController($routeParams, EventFactory) {
+    controller: ['$routeParams', 'EventFactory', '$resource', '$scope',
+        function UserController($routeParams, EventFactory, $resource, $scope) {
             var self = this;
             self.userId = $routeParams.userId;
             this.orderProp = 'date'; //TODO понять принцип сортировки
             self.events = EventFactory.getEvents({user_Id: this.userId});
-            self.places = EventFactory.getPlaces();
-            // self.selectedOption = self.places[0]; //TODO сделать выбором по умолчанию
-
-            self.title = '';
-            self.text = '';
-            self.startTime = '';
-            self.endTime = '';
-            self.place = '';
+            self.places = getPlaces();
 
             self.submit = submit;
 
             function submit() {
-                console.log(self.title);
-                console.log(self.text);
-                console.log(self.startTime);
-                console.log(self.endTime);
-                console.log(self.place);
-
-
-                /*EventFactory.createEvent({
+                //$scope.myModal.modal('hide'); //TODO понять как скрыть форму
+                $scope.eventCreateForm.$setPristine(); //TODO понять как очистить форму
+                self.title = '';
+                self.text = '';
+                self.startTime = '';
+                self.endTime = '';
+                EventFactory.createEvent({
                     id: null,
-                    title: self.event.title,
-                    text: self.event.text,
-                    startTime: self.event.startTime,
-                    endTime: self.event.endTime,
-                    place: self.event.place,
-                    owner: self.event.owner
+                    title: self.title,
+                    text: self.text,
+                    startTime: self.startTime,
+                    endTime: self.endTime,
+                    place: self.place,
+                    owner: self.userId
                 }, function (data) {
+                    $scope.eventCreateForm.$setPristine();
                     getEvents();
                 }, function (errResponse) {
                     console.error('Error while creating Event');
-                });*/
+                });
             }
 
             function getEvents() {
                 self.events = EventFactory.getEvents({user_Id: self.userId});
+            }
+
+            function getPlaces() {
+                return $resource('http://localhost:8083/getPlaces',
+                    {charge: {method: 'GET', isArray: true}}).query(function (data) {
+                        self.place = data[0].id;
+                        return data;
+                });
             }
         }]
 });
