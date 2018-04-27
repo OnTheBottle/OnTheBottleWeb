5fa1acf11b1b0ea09b94c9a82af603732b86467c
Fabolo <fabolo1234@gmail.com>
2018-04-27 17:51:31 +0300

    Added new type passed event and rules to sort

--- src/main/resources/static/components/events/events.component.js
+++ src/main/resources/static/components/events/events.component.js
@@ -11,7 +11,6 @@ angular.module('events').component('events', {
             var self = this;
             //self.userId = $routeParams.userId;
             self.options = {allEvents: 'true', activeEvents: false, passedEvents: false};
-            self.orderProp = 'date'; //TODO понять принцип сортировки
             self.today = new Date();
 
             self.event = {
