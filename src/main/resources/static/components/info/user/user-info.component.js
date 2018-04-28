0cad258f097c230b9b5347318ac9e421f1ac13b5
ctaxx <bykov.stanislav.p@gmail.com>
2018-04-29 02:20:49 +0300

    first attempts in user-info.component

--- src/main/resources/static/components/info/user/user-info.component.js
+++ src/main/resources/static/components/info/user/user-info.component.js
@@ -13,8 +13,21 @@
     function infoController($http) {
 
         var model = this;
-
+        model.requestData = {};
+        model.requestData.id = "0f24d8a4-9176-42a5-bc52-affb483e3308";
         model.$onInit = function () {
+        }
+        model.showUser = function(){
+            console.log("init works");
+            $http({
+                method: "GET",
+                url: USER_PATH + "/showUsers",
+                params: model.requestData
+            }).then(function mySuccess(response) {
+                console.log('response userInfo: ', response.data.name);
+                model.userName = response.data.name;
+            }, function myError(response) {
+            });
         }
 
     }
