465ff7c31acb6571d8d4404370f659e4008b95b9
Alexander Cherba <alexander.cherba@gmail.com>
2018-05-05 12:36:56 +0300

    fix Registration Component

--- src/main/resources/static/components/registration/registration.module.js
+++ src/main/resources/static/components/registration/registration.module.js
@@ -1,3 +1,52 @@
 'use strict';
 
-angular.module('registrationApp',['ngCookies']);
+angular.module('registrationApp', ['ngCookies']);
+
+
+(function () {
+    angular.module('registrationApp')
+        .directive('onlyLettersInput', onlyLettersInput);
+
+    function onlyLettersInput() {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attr, ngModelCtrl) {
+                function fromUser(text) {
+                    var transformedInput = text.replace(/[^a-zA-Z]/g, '');
+                    //console.log(transformedInput);
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+
+                ngModelCtrl.$parsers.push(fromUser);
+            }
+        };
+    }
+})();
+
+(function () {
+    angular.module('registrationApp')
+        .directive('onlyLettersAndNumbersInput', onlyLettersInput);
+
+    function onlyLettersInput() {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attr, ngModelCtrl) {
+                function fromUser(text) {
+                    var transformedInput = text.replace(/[^a-zA-Z0-9]/g, '');
+                    //console.log(transformedInput);
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+
+                ngModelCtrl.$parsers.push(fromUser);
+            }
+        };
+    }
+})();
