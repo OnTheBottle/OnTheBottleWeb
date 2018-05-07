b38956e69647977fcf7ab376be58318457c6f7f5
Alexander Cherba <alexander.cherba@gmail.com>
2018-05-07 21:05:54 +0300

    delete old index ang js files

--- src/main/resources/static/components/place/view/view-place.component.js
+++ src/main/resources/static/components/place/view/view-place.component.js
@@ -3,14 +3,14 @@
     angular.module('viewPlaceApp')
         .component('viewPlaceComp', {
             templateUrl: 'components/place/view/view-place.component.html',
-            controller: ['$http', '$cookies', '$route', viewController],
+            controller: ['$http', viewController],
             controllerAs: 'model',
             bindings: {
                 friend: '='
             }
         });
 
-    function viewController($http, $cookies, $route) {
+    function viewController($http) {
 
         var model = this;
 
