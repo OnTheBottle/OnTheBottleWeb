ad8b17219ba7b75c155381eba139445f10e3b4ac
Alexander Cherba <alexander.cherba@gmail.com>
2018-05-10 21:44:45 +0300

    more small fix

--- src/main/resources/static/js/masterApp.config.js
+++ src/main/resources/static/js/masterApp.config.js
@@ -6,14 +6,14 @@ angular.module('mainApp')
             $locationProvider.hashPrefix('!');
 
             $routeProvider.when('/news', {
-                template: '<news-comp user-id="ctrl.userId"></news-comp>'
+                template: '<notify-friend-request-comp user-id="ctrl.userId"></notify-friend-request-comp>' +
+                '<news-comp user-id="ctrl.userId"></news-comp>'
             }).when('/profile', {
                 template: '<profile-info-comp user-id="ctrl.userId"></profile-info-comp>'
             }).when('/userInfo:id', {
                 template: '<user-info-comp user-id="ctrl.userId"></user-info-comp>'
             }).when('/friend', {
-                template: '<notify-friend-request-comp user-id="ctrl.userId"></notify-friend-request-comp>' +
-                '<view-friends-comp user-id="ctrl.userId"></view-friends-comp>'
+                template: '<view-friends-comp user-id="ctrl.userId"></view-friends-comp>'
             }).when('/event', {
                 template: '<events-comp user-id="ctrl.userId"></events-comp>'
             }).when('/find', {
@@ -22,8 +22,6 @@ angular.module('mainApp')
                 template: '<find-comp user-id="ctrl.userId"></find-comp>'
             }).when('/wall', {
                 template: '<posts-comp></posts-comp>'
-            }).when('/friendmanual', {
-                template: '<link-friends-comp></link-friends-comp>'
             }).when('/place', {
                 template: '<view-place-comp>laceComp user-id="ctrl.userId></view-place-comp>'
             }).otherwise({
