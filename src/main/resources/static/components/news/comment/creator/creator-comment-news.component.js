40f9273d65d048a0ca860d46db62c57f6624c009
Alexander Cherba <alexander.cherba@gmail.com>
2018-05-14 12:04:03 +0300

    refactory the comments form to news

--- src/main/resources/static/components/news/comment/creator/creator-comment-news.component.js
+++ src/main/resources/static/components/news/comment/creator/creator-comment-news.component.js
@@ -3,34 +3,36 @@
     angular.module('creatorCommentNewsApp')
         .component('creatorCommentNewsComp', {
             templateUrl: 'components/news/comment/creator/creator-comment-news.component.html',
-            controller: ['$http', '$cookies', '$route', CreatorController],
+            controller: ['$http', '$cookies', CreatorController],
             controllerAs: 'model',
             bindings: {
-                postId: '=',
+                post: '<',
             }
         });
 
-    function CreatorController($http, $cookies, $route) {
+    function CreatorController($http, $cookies) {
 
         var model = this;
 
         model.$onInit = function () {
         }
 
-        model.addComment = function (text) {
+        model.addComment = function () {
             var access_token = $cookies.get('access_token');
             $http({
                 method: "POST",
                 url: MESSAGE_PATH + "/news/comment/add",
                 params: {
                     access_token: access_token,
-                    postId: model.postId,
-                    text: text
+                    postId: model.post.id,
+                    text: model.text
                 }
             }).then(function mySuccess(response) {
-                if (response.data === true) {
-                    console.log('Added new comment');
-                    $route.reload();
+                if (response.data != null) {
+                    var comment = response.data;
+                    model.post.comments.push(comment);
+                    model.text = '';
+                    model.post.commentCount++;
                 }
             }, function myError(response) {
                 console.log(response.statusText, 'addComment: Sorry, I am tired');
